682c1a625aca513f15989920e5c210d176f20bb9
Herman.Xinhe.Guo <hguo@c2micro.com>
2011-03-04 12:06:54 +0800

    commit at Fri Mar  4 12:06:54 CST 2011

--- ltp-run.sh
+++ ltp-run.sh
@@ -1,45 +1,16 @@
 #!/bin/sh
-
-THISTID=`date +%Y%m%d%H%M%S`
-THISMAC=`/sbin/ifconfig eth0 | grep 'HWaddr' | sed 's/.*HWaddr \(.*\)/\1/'`
 THISIP=`/sbin/ifconfig eth0 | grep 'inet addr' | sed 's/.*addr:\(.*\)\(  Bcast.*\)/\1/'`
-THISUSR=`whoami`
-THISHOST=`uname -n`
-THISKV=`uname -v`
-THISARGC=$#
-THISARGV=$@
 
-func_nfs(){
-    mdone=`cat /etc/mtab |  grep "$1 "`
-    if [ "$mdone" != "" ]; then
-        echo Already mounted $1 #: $mdone
-        return 0
-    fi
+mkdir -p /local /home/work /.ssh
+mount -o nolock -t nfs 10.16.8.4:/local /local
+[ -f /.ssh/authorized_keys ] || cp -f /local/c2/authorized_keys /.ssh;
+[ -f /local/hguo/nfsroot/autoltp/ltpstatus ] && . /local/hguo/nfsroot/autoltp/ltpstatus
+d=`diff /home/work/run.sh $ltp_runsh`
+[ "$d" != "" ] && (cp -f $ltp_runsh /home/work/run.sh;sync;)
 
-    host=${1%%:*}
-    hostdir=${1##*:}
-    #do not mount itself, loop mount, dangerous
-    [ "$host" == "$(hostname)" ] && return 0 
-    [ "$host" == "$THISIP" ] && return 0
-
-    mkdir -p $2
-    mount -o nolock -t nfs $1 $2
-    echo mount nfs $1 --\> $2
-}
-ltp_test()
-{
-    mkdir -p /local /home/work /.ssh
-    func_nfs 10.16.8.4:/local                 /local
-    [ -f /.ssh/authorized_keys ] || cp -f /local/c2/authorized_keys /.ssh;
-    [ -f /local/hguo/nfsroot/autoltp/ltpstatus ] && . /local/hguo/nfsroot/autoltp/ltpstatus
-    d=`diff /home/work/run.sh $ltp_runsh`
-    [ "$d" != "" ] && (cp -f $ltp_runsh /home/work/run.sh;sync;echo "$ltp_runsh updated";)
-    mkdir -p $ltp_result
-    echo $THISIP >$ltp_clientip
-    rm -f $ltp_doneflag
-    cd $ltp_release
-    ./$ltp_testcmd -p -q  -l $ltp_result/resultlog.$ltp_kernelbuild -d $ltp_result
-    echo "done" > $ltp_doneflag
-}
-
-ltp_test
+mkdir -p $ltp_result
+echo $THISIP >$ltp_clientip
+rm -f $ltp_doneflag
+cd $ltp_release
+./$ltp_testcmd -p -q  -l $ltp_resultlog -d $ltp_result
+echo "done" > $ltp_doneflag
