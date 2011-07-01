a49b177bd63c00fb1d361f19326d9b19e273c4c1
hguo <hguo@c2micro.com>
2011-07-01 11:59:44 +0800

    make repoclone.sh generic

--- repoclone-c2sdk.sh
+++ repoclone-c2sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d .repo ]; then
+if [ ! -d .repo/repo ]; then
 #this will create .repo/repo
 mkdir -p .repo;
 pushd .repo;
@@ -11,10 +11,6 @@ fi
 if [ ! -d .repo/manifests ]; then
 #this will create in .repo/ manifest.xml  manifests  manifests.git 
 repo init -u ssh://git.bj.c2micro.com/c2sdk/manifests.git
-echo please restart this command to sync all data.
-echo this second time will run a long time download data.
-echo suggest put to a background task.
-exit 0
 fi
 
 if [ -f .repo/project.list ]; then
--- repoclone.sh
+++ repoclone.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-#goes to target folder and clone a repo from local, otherwise will from website.
-if [ ! -d .repo ]; then
+BR=${1:-devel}
+urlrepo=ssh://git.bj.c2micro.com/mentor-mirror/build/repo.git
+urlmani=ssh://git.bj.c2micro.com/mentor-mirror/build/manifests.git
+if [ ! -d .repo/repo ]; then
 mkdir -p .repo;
 pushd .repo;
-git clone ssh://git.bj.c2micro.com/mentor-mirror/build/repo.git;
+git clone $urlrepo
 popd;
 fi
 
-BR=${1:-devel}
-
-if [ -d .repo/manifests ]; then
-repo start $BR --all
-else
-repo init -u ssh://git.bj.c2micro.com/mentor-mirror/build/manifests.git -b $BR
+if [ ! -d .repo/manifests ]; then
+repo init -u $urlmani -b $BR
 fi
+
+if [ -f .repo/project.list ]; then
+repo start $BR --all
+fi
+
 repo sync
 repo start $BR --all
