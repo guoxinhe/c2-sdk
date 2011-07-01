0eb986e3e0270046c18853ec99b3ff25ecd2a931
hguo <hguo@c2micro.com>
2011-07-01 11:19:46 +0800

    git auto commit on Delllinux by hguo, Fri Jul  1 11:19:46 CST 2011

--- repoclone.sh
+++ repoclone.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-BR=${1:-devel}
-
 #goes to target folder and clone a repo from local, otherwise will from website.
 if [ ! -d .repo ]; then
-mkdir -p .repo; 
-pushd .repo; 
-git clone ssh://git.bj.c2micro.com/mentor-mirror/build/repo.git; 
+mkdir -p .repo;
+pushd .repo;
+git clone ssh://git.bj.c2micro.com/mentor-mirror/build/repo.git;
 popd;
 fi
 
+BR=${1:-devel}
+
+if [ -d .repo/manifests ]; then
+repo start $BR --all
+else
 repo init -u ssh://git.bj.c2micro.com/mentor-mirror/build/manifests.git -b $BR
+fi
 repo sync
 repo start $BR --all
