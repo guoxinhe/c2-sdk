edee6fc143762bf239c167385972a5d1c0132ab0
hguo <hguo@c2micro.com>
2011-09-07 11:05:53 +0800

    optimize repoclone.sh

--- repoclone.sh
+++ repoclone.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
 BR=${1:-devel}
-urlrepo=ssh://git.bj.c2micro.com/mentor-mirror/build/repo.git
-urlmani=ssh://git.bj.c2micro.com/mentor-mirror/build/manifests.git
-if [ ! -d .repo/repo ]; then
-mkdir -p .repo;
-pushd .repo;
-git clone $urlrepo
-popd;
+mf=${2:-default.xml}
+
+if [ ! -d .repo ]; then
+mkdir -p .repo; git clone ssh://git.bj.c2micro.com/mentor-mirror/build/repo.git .repo/repo
 fi
 
-if [ ! -d .repo/manifests ]; then
-repo init -u $urlmani -b $BR
-fi
-
-if [ -f .repo/project.list ]; then
+if [ -d .repo/manifests ]; then
 repo start $BR --all
+else
+yes "" | repo init -u ssh://git.bj.c2micro.com/mentor-mirror/build/manifests.git -b $BR -m $mf
 fi
-
 repo sync
 repo start $BR --all
