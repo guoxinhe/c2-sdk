d0649baf8e4d6ffe3153cc1d0c0548475a9d9f3d
hguo <hguo@c2micro.com>
2011-09-29 11:16:36 +0800

    make bash_treepid.sh kill works

--- bash_treepid.sh
+++ bash_treepid.sh
@@ -36,59 +36,40 @@ echo_head()
         echo -en "|   "
     done   
     echo -en "|-- "
-
 )}
 
-MYKILL()
+MYSHOW()
 {(
     cmd=
     if test $CONFIG_SHOW_CMD -eq 1 ; then
-        #cmd=`ps $1 | grep $1 | sed 's/.* \(.*\)/\1/g'`
-        cmd=`ps $1 | grep $1 | awk '{print $5 }'`
+        cmd=`ps $1 | grep $1 | awk '{print $5 " " $6 " " $7 " " $8 " " $9 }'`
     fi
     echo $1 $cmd
-    #kill -9 $1 >/dev/null 2>&1
 )}
-MYKILL $sub0
-for sub1 in `ps -ef| awk '$3 == '$sub0' { print $2 }'` ; do level=0;echo_head;MYKILL $sub1;
-for sub2 in `ps -ef| awk '$3 == '$sub1' { print $2 }'` ; do level=1;echo_head;MYKILL $sub2;
-for sub3 in `ps -ef| awk '$3 == '$sub2' { print $2 }'` ; do level=2;echo_head;MYKILL $sub3;
-for sub4 in `ps -ef| awk '$3 == '$sub3' { print $2 }'` ; do level=3;echo_head;MYKILL $sub4;
-for sub5 in `ps -ef| awk '$3 == '$sub4' { print $2 }'` ; do level=4;echo_head;MYKILL $sub5;
-for sub6 in `ps -ef| awk '$3 == '$sub5' { print $2 }'` ; do level=5;echo_head;MYKILL $sub6;
-for sub7 in `ps -ef| awk '$3 == '$sub6' { print $2 }'` ; do level=6;echo_head;MYKILL $sub7;
-for sub8 in `ps -ef| awk '$3 == '$sub7' { print $2 }'` ; do level=7;echo_head;MYKILL $sub8;
-for sub9 in `ps -ef| awk '$3 == '$sub8' { print $2 }'` ; do level=8;echo_head;MYKILL $sub9;
-done;done;done;done;done;done;done;done;done;
-
+MYKILL()
+{(
+    if test $CONFIG_KILL_CMD -eq 1 ; then
+        kill -9 $1 >/dev/null 2>&1
+    fi
+)}
+MYSHOW $sub0;
+ for sub1 in `ps -ef| awk '$3 == '$sub0' { print $2 }'` ; do level=0;echo_head;MYSHOW $sub1;
+  for sub2 in `ps -ef| awk '$3 == '$sub1' { print $2 }'` ; do level=1;echo_head;MYSHOW $sub2;
+   for sub3 in `ps -ef| awk '$3 == '$sub2' { print $2 }'` ; do level=2;echo_head;MYSHOW $sub3;
+    for sub4 in `ps -ef| awk '$3 == '$sub3' { print $2 }'` ; do level=3;echo_head;MYSHOW $sub4;
+     for sub5 in `ps -ef| awk '$3 == '$sub4' { print $2 }'` ; do level=4;echo_head;MYSHOW $sub5;
+      for sub6 in `ps -ef| awk '$3 == '$sub5' { print $2 }'` ; do level=5;echo_head;MYSHOW $sub6;
+       for sub7 in `ps -ef| awk '$3 == '$sub6' { print $2 }'` ; do level=6;echo_head;MYSHOW $sub7;
+        for sub8 in `ps -ef| awk '$3 == '$sub7' { print $2 }'` ; do level=7;echo_head;MYSHOW $sub8;
+         for sub9 in `ps -ef| awk '$3 == '$sub8' { print $2 }'` ; do level=8;echo_head;MYSHOW $sub9;
+         MYKILL $sub9;done;
+        MYKILL $sub8;done;
+       MYKILL $sub7;done;
+      MYKILL $sub6;done;
+     MYKILL $sub5;done;
+    MYKILL $sub4;done;
+   MYKILL $sub3;done;
+  MYKILL $sub2;done;
+ MYKILL $sub1;done;
+MYKILL $sub0;
 exit 0
-
-#exit 0;
-#
-#arraycounter=1
-#while true
-#do
-#        FORLOOP=FALSE
-#        # Get all the child process id
-#        for i in `ps -ef| awk '$3 == '$ppid' { print $2 }'`
-#        do
-#                if [ $i -ne $CURPID ] ; then
-#                        procid[$arraycounter]=$i
-#                        arraycounter=`expr $arraycounter + 1`
-#                        ppid=$i
-#                        FORLOOP=TRUE
-#                fi
-#        done
-#        if [ "$FORLOOP" = "FALSE" ] ; then
-#           arraycounter=`expr $arraycounter - 1`
-#           ## We want to kill child process id first and then parent id's
-#           while [ $arraycounter -ne 0 ]
-#           do
-#             echo "Killing procid[$arraycounter]=${procid[$arraycounter]}"
-#             kill -9 "${procid[$arraycounter]}" >/dev/null
-#             arraycounter=`expr $arraycounter - 1`
-#           done
-#         exit
-#        fi
-#done
-#
